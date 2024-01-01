a5e053f1928d5d4191b5bf62c7315380a7e1a1ac
abhishekghanekar13 <142926791+abhishekghanekar13@users.noreply.github.com>
2024-01-01 13:33:05 +0530

    I replace the first character to capital letter

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,7 @@ let Heading = () => {
   return (
     <>
       <h1 className={styles.heading}>TODO-List-APP</h1>
-      <p>this app is very helpFull For student</p>
+      <p> This App Is Very HelpFull For Student</p>
     </>
   );
 };
