151ef1b65f04080e85f2a43c143e82aaf0496594
abhishekghanekar13 <142926791+abhishekghanekar13@users.noreply.github.com>
2024-01-01 12:53:40 +0530

    I change the heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import styles from "./Heading.module.css"
 let Heading=()=>{
     return(<>
-    <h1 className={styles.heading}>TODO-APP</h1>
+    <h1 className={styles.heading}>TODO-List-APP</h1>
     </>)
 }
 export default Heading;
